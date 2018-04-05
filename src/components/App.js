66d2eb5c8ec2782e78d113fd6facabb9054f49ab
rfarney3 <farneyr@yahoo.com>
2018-04-05 15:45:00 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
+import Data from "../porkers_data.js"
+import HogList from "./HogList.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           < Nav />
-
+          < HogList data={Data} />
       </div>
     )
   }

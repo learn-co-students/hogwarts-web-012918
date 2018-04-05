c907a41d7a6ef03860bd042ea69688356cfeaec1
scott <you@example.comitsmescottb123@gmail.com>
2018-04-05 16:19:50 -0400

    Done.

---
+++ src/components/Filtered.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+//
+// export default class Filtered extends React.Component {
+//
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
---
+++ src/components/HogList.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import HogCard from './HogCard'
+
+
+export default class HogList extends React.Component{
+constructor(props){
+  super(props)
+    this.state = {
+      hogs: props.hogsData,
+      filtered: 'all'
+  }
+}
+
+  handleFilterValue = (event) => {
+    event.preventDefault()
+    let currentValue= event.target.value
+    if (currentValue === 'name') {
+      this.setState({
+        filtered: 'name'
+      })
+    } else if (currentValue === 'weight') {
+      this.setState({
+        filtered: 'weight'
+      })
+    } else if(currentValue === 'greased') {
+      this.setState({
+        filtered: 'greased'
+      })
+    } else {
+      this.setState({
+        filtered: 'all'
+      })
+    }
+  }
+
+  filterHogs = () => {
+    if (this.state.filtered === 'all') {
+      return this.state.hogs.map((hog) => <HogCard hog={hog} key={hog.name}/>)
+    } else if(this.state.filtered === 'name') {
+        return this.state.hogs.sort((a, b) => ( a.name < b.name )?-1:1).map((hog) => <HogCard hog={hog} key={hog.name}/>)
+      } else if(this.state.filtered === 'weight') {
+          let weight = 'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+          return this.state.hogs.sort((a, b) => ( a[weight] - b[weight] )).map((hog) => <HogCard hog={hog} key={hog.name}/>)
+        } else {
+          return this.state.hogs.filter((hog) => hog.greased === true).map((hog) => <HogCard hog={hog} key={hog.name}/>)
+        }
+    }
+
+
+
+  render(){
+  return(
+    <div>
+
+      <div>
+        <select value={this.props.filtered} onChange={this.handleFilterValue}>
+          <option value="all">All</option>
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+          <option value="greased">Greased</option>
+        </select>
+      </div>
+      <p> </p>
+      <div className="ui grid container">
+        {this.filterHogs()}
+      </div>
+
+    </div>
+  )
+  }
+
+
+
+
+
+}

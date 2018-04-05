7d407343e276a230e931c0c9936260949ba7a084
github username <github email address>
2018-04-05 15:26:39 -0400

    switch

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -5,6 +5,32 @@ import Hog from './Hog'
 export default class HogList extends React.Component {
 
 
+  sortHogs = (filterSort) => {
+    const weight = "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water";
+    if(filterSort.nameSort) {
+      return AllHogData.sort((hogA, hogB) => {
+        return hogA.name.localeCompare(hogB.name)
+        // return(<div className="ui eight wide column"><Hog
+        //   hogData={hog}
+        //   id={index + 1}
+        // /></div>)
+      })
+    } else if (filterSort.weightSort) {
+      return AllHogData.sort((hogA, hogB) => {
+        return hogA[weight] - hogB[weight]
+        // return(<div className="ui eight wide column"><Hog
+        //   hogData={hog}
+        //   id={index + 1}
+        // /></div>)
+      })
+    }
+  }
+
+  filterHogs = (filterSort) => {
+
+  }
+
+
   getAllHogNames = () => {
     return AllHogData.map((hog, index) => {
       return(<div className="ui eight wide column" key={index+1}><Hog
@@ -15,7 +41,8 @@ export default class HogList extends React.Component {
   }
 
   render() {
-
+    this.sortHogs(this.props.filterSort)
+    this.filterHogs(this.props.filterSort)
     return (
       <div className="ui grid container">
 
